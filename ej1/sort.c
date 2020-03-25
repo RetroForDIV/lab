12755fea8f1298b244a3208cbffd244aff555863
Mati Rojas <retrofordiv@gmail.com>
2020-03-25 15:10:22 -0400

    we

--- ej1/sort.c
+++ ej1/sort.c
@@ -27,24 +27,9 @@ void insertion_sort(int a[], unsigned int length) {
 
 //gcc -Wall -Werror -Wextra -pedantic -std=c99 -c array_helpers.c sort.c
 //gcc -Wall -Werror -Wextra -pedantic -std=c99 -o sorter *.o main
-//./sorter input/example-unsorted.in
 
-/*   
+/*    */
 
-proc insertion_sort (in/out a: array[1..n] of T)
-for i:= 2 to n do
-insert(a,i)
-od
-end proc
-proc insert (in/out a: array[1..n] of T, in i: nat)
-var j: nat
-j:= i
-do j > 1 ^ a[j] < a[j − 1] ! swap(a,j-1,j)
-j:= j-1
-od
-end proc
-
- */
 
 
 
