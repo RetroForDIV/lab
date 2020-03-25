31c67676d86cb8311a1a25b15290fcbaf8ccf818
retrofordiv <retrofordiv@gmail.com>
2020-03-25 16:15:22 -0300

    ejej

--- ej1/sort.c
+++ ej1/sort.c
@@ -27,7 +27,6 @@ void insertion_sort(int a[], unsigned int length) {
 //./sorter input/example-unsorted.in
 
 /*   
-
 proc insertion_sort (in/out a: array[1..n] of T)
 for i:= 2 to n do
 insert(a,i)
@@ -40,7 +39,6 @@ do j > 1 ^ a[j] < a[j − 1] ! swap(a,j-1,j)
 j:= j-1
 od
 end proc
-
  */
 
 
