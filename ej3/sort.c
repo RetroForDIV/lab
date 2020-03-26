8a3e23634aba12ee98bb59c92af99519de368d6c
Mati Rojas <retrofordiv@gmail.com>
2020-03-26 12:24:55 -0400

    com

--- ej3/sort.c
+++ ej3/sort.c
@@ -9,6 +9,7 @@
 
 
 static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
+
     /* Needs implementation */
 
     /* Permutes elements of a[izq..der] and returns pivot such that:
@@ -38,27 +39,9 @@ void quick_sort(int a[], unsigned int length) {
 
 
 
- /* 
+ /*
 
-proc quick_sort_rec (in/out a: array[1..n] of T, in lft,rgt: nat)
-var ppiv: nat
-if rgt > lft ! partition(a,lft,rgt,ppiv)
-lft ≤ ppiv ≤ rgt
-elementos en a[lft,ppiv-1] ≤ a[ppiv]
-elementos en a[ppiv+1,rgt] ≥ a[ppiv]}
-quick_sort_rec(a,lft,ppiv-1)
-quick_sort_rec(a,ppiv+1,rgt)
-fi
-end proc
-
---------------------------------------------------------------------------
-
-unsigned int partition(int a[], unsigned int izq, unsigned int der);
-
- Permutes elements of a[izq..der] and returns pivot such that:
-     - izq <= pivot <= der
-     - elements in a[izq,pivot) all 'go_before' a[pivot]
-     - a[pivot] 'goes_before' all the elements in a(pivot,der]
+ 
 
 //     gcc -Wall -Werror -Wextra -pedantic -std=c99 -no-pie -c array_helpers.c sort.c
 //     gcc -Wall -Werror -Wextra -pedantic -std=c99 -no-pie -o sorter *.o main
